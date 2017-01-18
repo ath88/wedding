49b752e5e9df63f36488d62e7aeed805fc7c908c
Asbjørn Thegler <asbjoern@gmail.com>
2017-01-18 16:36:40 +0100

    Add stuff

--- js/agency.js
+++ js/agency.js
@@ -19,7 +19,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
 
@@ -31,3 +31,36 @@
     })
 
 })(jQuery); // End of use strict
+
+var places = {
+    vielse: new google.maps.LatLng(55.769348, 12.451729),
+    reception: new google.maps.LatLng(55.7717256, 12.4507801),
+    fest: new google.maps.LatLng(55.016741, 11.875907),
+    overnatning1: new google.maps.LatLng(55.008302, 11.911308),
+    overnatning2: new google.maps.LatLng(55.024054, 11.898026)
+};
+
+for (var place in places) {
+    var position = places[place];
+
+    var mapCanvas = document.getElementById(place);
+    var mapOptions = {
+        center: position,
+        zoom: 15,
+        scrollwheel: false,
+        draggable: false,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        overviewMapControl: false,
+        rotateControl: false,
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+}
